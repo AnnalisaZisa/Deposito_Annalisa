6106cf334c2528e5c78ea94ba151be33e4e3ad80
AnnalisaZisa <annalisa.zisa@acus.eu>
2025-04-10 12:56:11 +0200

    Add files via upload

---
+++ 1004/es1_pandas.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+names_arr = np.array(['Marco', 'Giulia', 'Francesco', 'Simone', 'Carla', 'Vittoria', 'Cesare'])
+cities_arr = np.array(['Milano', 'Pavia', 'Torino', 'Roma', 'Palermo'])
+age = np.linspace(0, 30, 5)
+salary = np.linspace(0, 2000, 5)
+
+
+names = []
+cities = []
+while len(names) < 6: 
+
+    names.append(np.random.choice(names_arr, size = 6))
+    cities.append(np.random.choice(cities_arr, size = 6))
+
+    break
+
+dataset = {'Nome': names, 'Eta': age, 'Citta': cities, 'Salario': salary}
+
+df = pd.DataFrame(dataset)        
+print(df)
+    
+
+
+
